000963a55927b3a9152200ad99809adeca4c7ad5
Dharmik <dharmik.rathod@vedicrishiastro.com>
2022-01-23 12:04:57 +0530

    complete index page

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import MobileNav from "../header/mobileNav";
 import TopNav from "../header/topnav";
 import BannerCarousel from "../swiper/bannerSwiper";
-import Navbar from "../header/navbar";
 import Footer from "../footer/footer";
 
 export default function Layout(props) {
