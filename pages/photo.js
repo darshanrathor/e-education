70e007d53027dd2e257e7af8d750b60595ec549c
Dharmik <dharmik.rathod@vedicrishiastro.com>
2022-02-21 09:15:05 +0530

    Gallery

--- pages/photo.js
+++ pages/photo.js
@@ -2,20 +2,29 @@ export default function photo() {
 return(
 
     <>
+   <div className=" items-center md:pt-32 md:pb-32 pt-20 pb-10  ">
+   <h1 className=" text-bold gap-10 text-center text-b py-8 "> OUR GALLEY </h1>
+   
+        <div className="  px-5  grid  md:gap-2 max-w-9xl mx-auto">
+          <div className=" items-center w-full grid sm:grid-cols-2 md:grid-cols-3 gap-2">
 
-          <div className=" bg-black mx-auto bg-cover py- ">
-          
-
-
-
-          <div className="grid max-w-6xl mx-auto sm:grid-cols-2 grid-cols-1 md:grid-cols-3 gap-8 " >
+            
         
           <img src="\gallery\1.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\3.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\4.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\5.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\6.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\7.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\8.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\9.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\10.jpeg " class=" inline first-letter:"></img>
+          <img src="\gallery\11.jpeg " class=" inline first-letter:"></img>
 
 
 
           </div>
-
+          </div>
 
 
 </div>
